bcde8ead5cda122b1b5a3a907db0e6392260b116
Antonio Gatto <antonio.work@hotmail.com>
2011-08-08 11:35:08 -0300

    Implementacao das validacoes e teste

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,13 @@
 class User < ActiveRecord::Base
   
   attr_accessor :nome, :email
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence => {:is => true, :message => " - nao foi preenchido."},
+                   :length  => {:minimum => 1, :maximium => 40, :message => " - tamanho incorreto."};
+
+  validates :email, :presence => {:is => true, :message => "- nao foi preenchido."},
+                    :length => {:minimum => 1, :maximium => 40, :message => "- tamanho incorreto."},
+                    :format => {:with => email_regex, :message => " - formato invalido"},
+                    :uniqueness => {:case_sensitive => false, :message => " - esse email já foi cadastrado"};
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,29 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 
+  before(:each) do
+
+    @attr = {:name => "Teste Sistema", :email => "teste@teste.com"}
+  
+  end
+
+  it "should create a new instance  given valid attributes" do
+    User.create!(@attr)
+  end
+
+  #it "should reject invalid email addresses" do
+   # addresses = %w[user@foo.com]
+   # addresses.each do |address|
+   #   invalid_email_user = User.new(@attr.merge(:email => address))
+   #   invalid_email_user.should_not be_valid
+  #end   
+  #end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
 end
